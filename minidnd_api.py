02efc5d21bb2e4cf2af2478a89cf7ff50676c45c
daniel <daniel@meshconsultants.ca>
2022-05-09 22:19:10 -0400

    api with flask

---
+++ minidnd_api.py
@@ -0,0 +1,31 @@
+import flask
+from flask import jsonify
+from flask import render_template, redirect, url_for, request, flash, send_from_directory
+from flask_cors import CORS
+from flask_sqlalchemy import SQLAlchemy
+import os
+import json
+from werkzeug.utils import secure_filename
+from werkzeug.security import generate_password_hash, check_password_hash
+import subprocess
+import shutil
+import nanoid
+import math  
+import random
+import datetime
+
+ALLOWED_EXTENSIONS = {'spec', 'obj', 'stl', 'ply', 'txt', 'png', 'jpg', 'jpeg', 'gif'}
+
+app = flask.Flask(__name__, static_url_path='', static_folder='.next')
+CORS(app)
+
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+app.config['SECRET_KEY'] = 'X0UQR7OkPcN2zAhdwAf5HU2G9tIGip31'
+app.config['MAX_CONTENT_LENGTH'] = 1e10
+app.config['SQLALCHEMY_DATABASE_URI']= 'sqlite:////home/daniel/Dev/minidnd-r3f/minidnd_data.sqlite3'
+charset = '1234567890abcdefghijklmnopqrstuvwxyz'
+db = SQLAlchemy(app)
+
+@app.route("/", methods=['GET', 'POST'])
+def index():
+    return send_from_directory(app.static_folder,'index.html')
---
+++ wsgi.py
@@ -0,0 +1,6 @@
+from minidnd_api import app
+from minidnd_api import db
+
+if __name__ == "__main__":
+    db.create_all()
+    app.run(ssl_context='adhoc', threaded=True)
